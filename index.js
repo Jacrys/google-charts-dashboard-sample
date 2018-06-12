c64fd841fa859fae8475732969de47d5608f7019
Keith Lewis <keithlewis@trinitycomputers.net>
2018-06-12 12:08:39 -0500

    Got Static Loads to work
    Made static files load inline

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 // JavaScript Document
-$(document).ready(function(){ $("#tab-areachart").click(function(){ $('areachart').load('Module1/AreaChart.html');
+$(document).ready(
 
-}); 
-  
-  });
+  function(){ $("#tab-areachart").click(function() {
+    $('#areachart').load('Module1/AreaChart.html');
+
+    });
+
+  }
+
+  function(){ $("#tab-piechart").click(function() {
+    $('#piechart').load('Module1/PieChart.html');
+
+    });
+
+  }
+
+  function(){ $("#tab-dataview").click(function() {
+    $('#dataview').load('Module1/DataView.html');
+
+    });
+
+  }
+
+  function(){ $("#tab-dataquery").click(function() { 
+    $('#dataquery').load('Module1/DataQuery.html');
+
+    });
+
+  }
+
+);
